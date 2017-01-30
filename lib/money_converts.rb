db131d1c3314d4a97372da98e844ce8d5ecaa708
Andres Piedrahita <andresfph1989@gmail.com>
2017-01-30 13:25:59 +0100

    Create functionality to convert currencies

--- lib/money_converts.rb
+++ lib/money_converts.rb
@@ -8,10 +8,21 @@ class MoneyConverts
   end
 
   def self.conversion_rates(base_currency, rates)
-
+    @@base_currency = base_currency
+    @@rates = rates
   end
 
   def convert_to(to_currency)
+    if currency == @@base_currency
+      value = amount * @@rates[to_currency]
+      MoneyConverts.new(value, to_currency)
+    elsif currency != @@base_currency && to_currency == @@base_currency
+      value = amount / @@rates[currency]
+      MoneyConverts.new(value, to_currency)
+    else
+      base_currency = self.convert_to(@@base_currency)
+      base_currency.convert_to(to_currency)
+    end
   end
 
 end
--- spec/spec/money_converts_spec.rb
+++ spec/spec/money_converts_spec.rb
@@ -2,18 +2,22 @@ require_relative 'spec_helper'
 
 describe MoneyConverts do
   describe '.convert_to' do
-    it 'converts from eur to usd' do
-
+    it 'converts from a currency to another' do
       MoneyConverts.conversion_rates('EUR', {
         'USD'     => 1.11,
         'Bitcoin' => 0.0047
       })
 
-      fifty_euros = MoneyConverts.new(50, 'EUR');
+      fifty_euros = MoneyConverts.new(50, 'EUR')
+      twenty_dollars = MoneyConverts.new(20, 'USD')
 
-      result = 50 * 1.11
+      eur_to_dollar = 50 * 1.11
+      dollar_to_eur = 20 / 1.11
+      dollar_to_bitcoin = 20 / 1.11 * 0.0047
 
-      expect(fifty_euros.convert_to('USD').amount).to eq result
+      expect(fifty_euros.convert_to('USD').amount).to eq eur_to_dollar
+      expect(twenty_dollars.convert_to('EUR').amount).to eq dollar_to_eur
+      expect(twenty_dollars.convert_to('Bitcoin').amount).to eq dollar_to_bitcoin
     end
   end
 end
